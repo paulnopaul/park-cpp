141de927be72e0f31a93fbd539acbcb43e303c42
Paul <tscheklin@gmail.coj>
2020-10-15 14:50:20 +0300

    removed commented code

--- iz1/src/alg.c
+++ iz1/src/alg.c
@@ -5,7 +5,7 @@ int str_find(const char *str, char c)
     int i = 0;
     while (str[i] != c && str[i] != '\0')
         ++i;
-    return i; // str[i] == c ? i : -1;
+    return i;
 }
 
 int str_first_char_occurence(const char *str, const char *c)
